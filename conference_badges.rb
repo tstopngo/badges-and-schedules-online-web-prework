fbd880f76e5da0c03231e3e68ed929a32e114e9a
Tracy Ngo <tngo1206@gmail.com>
2019-02-08 04:41:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 array_speakers= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  rooms = [1,2,3,4,5,6,7]
+  
   
 def batch_badge_creator(array_speakers)
   badge_with_name =[]
