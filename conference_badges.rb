b4562bf55ab259ca2f6c41e5bc7f3f20e8aa0cf1
Tracy Ngo <tngo1206@gmail.com>
2019-02-08 04:04:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,10 @@ def badge_maker(name)
 end
 
 conference_attendees= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-badge_name =[]
+ badge_name =[]
+ 
 def batch_badge_creator(conference_attendees)
+  
   conference_attendees.each do |name|
     badge_maker(name)
 end
